893dcfd7389167726ae293995e6e614abc649aa5
stobrien89 <stobrien@crimson.ua.edu>
2021-09-11 13:48:36 -0700

    movement working

--- src/hooks/useKeyboardControls.js
+++ src/hooks/useKeyboardControls.js
@@ -1,62 +1,69 @@
-import { useState, useEffect } from "react";
-import { useStore } from "./useStore"
+import { useState, useEffect } from 'react';
+import { useStore } from './useStore';
 
-const actionByKey = key => {
-    const keys = {
-        keyW: "moveForward",
-        keyS: "moveBackward",
-        keyA: "moveLeft",
-        keyD: "moveRight",
-        Space: 'jump'
-    }
-
-    return keys[key];
+function actionByKey(key) {
+  const keys = {
+    KeyW: 'moveForward',
+    KeyS: 'moveBackward',
+    KeyA: 'moveLeft',
+    KeyD: 'moveRight',
+    Space: 'jump',
+  };
+  return keys[key];
 }
 
-const textureByKey = key => {
-    const keys = {
-        Digit1: 'dirt',
-        Digit2: 'grass',
-        Digit3: 'glass',
-        Digit4: 'wood',
-        Digit5: 'log'
-    }
-
-    return keys[key];
+function textureByKey(key) {
+  const keys = {
+    Digit1: 'dirt',
+    Digit2: 'grass',
+    Digit3: 'glass',
+    Digit4: 'wood',
+    Digit5: 'log',
+  };
+  return keys[key];
 }
-
 export const useKeyboardControls = () => {
-    const [movement, setMovement] = useState({
-        moveForward: false,
-        moveBackward: false,
-        moveLeft: false,
-        moveRight: false,
-        jump: false
-    });
+  const [movement, setMovement] = useState({
+    moveForward: false,
+    moveBackward: false,
+    moveLeft: false,
+    moveRight: false,
+    jump: false,
+  });
+  const [setTexture] = useStore((state) => [state.setTexture]);
 
-    const setTexture = useStore((state) => [state.setTexture])
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      // Movement key
+      if (actionByKey(e.code)) {
+        setMovement((state) => ({
+          ...state,
+          [actionByKey(e.code)]: true,
+        }));
+      }
+      // Change texture key
+      if (textureByKey(e.code)) {
+        setTexture(textureByKey(e.code));
+      }
+    };
+    const handleKeyUp = (e) => {
+      if (actionByKey(e.code)) {
+        setMovement((state) => ({
+          ...state,
+          [actionByKey(e.code)]: false,
+        }));
+      }
+    };
 
-    useEffect(() => {
-        const handleKeyDown = e => {
-            if (actionByKey(e.code)) {
-                setMovement((state) => ({...state, [actionByKey(e.code)]: true}))
-            }
-            if (textureByKey(e.code)) {
-                setTexture(textureByKey(e.code));
-            }
-        }
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
 
-        const handleKeyUp = e => {
-            if (actionByKey(e.code)) {
-                setMovement((state) => ({...state, [actionByKey(e.code)]: false}))
-            }
-        }
-        document.addEventListener('keydown', handleKeyDown)
-        document.addEventListener('keyup', handleKeyUp)
-        return () => {
-            document.removeEventListener('keydown', handleKeyDown)
-            document.removeEventListener('keyup', handleKeyUp)
-        }
-    })
-    return movement;
-}
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [setTexture]);
+
+  return movement;
+};
+  
