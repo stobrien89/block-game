aeb2114d3b58728f4ef91e321a934ffa8bf1fa97
stobrien89 <stobrien@crimson.ua.edu>
2021-09-16 18:28:46 -0700

    add hover state to cube

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useBox } from 'use-cannon';
 import * as textures from "../textures";
 
 export const Cube = ({ position, type, ...props}) => {
+    const [ hover, setHover ] = useState(null);
     const [ref] = useBox(() => ({
         type: 'Static',
         position,
@@ -10,12 +11,18 @@ export const Cube = ({ position, type, ...props}) => {
     }));
  
     return (
-     <mesh castShadow ref={ref}>
+     <mesh castShadow ref={ref}
+        onPointerMove={(e) => {
+            e.stopPropagation();
+            setHover(Math.floor(e.faceIndex / 2))
+        }}
+     >
          {[...Array(6)].map((_, index) => (
              <meshStandardMaterial 
                 attachArray="material"
                 map={textures[type]}
                 key={index}
+                color={hover === index ? "gray" : 'white'}
              />
          ))}
         <boxBufferGeometry attach="geometry" />
