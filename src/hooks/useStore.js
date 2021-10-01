ac5164cee4f9a42023598dbf73d7a56e360b69c2
stobrien89 <stobrien@crimson.ua.edu>
2021-09-30 18:30:28 -0700

    refactor usestore hook

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,14 +1,19 @@
 import create from 'zustand';
+import { nanoid } from 'nanoid';
 
 const getLocalStorage = key => JSON.parse(window.localStorage.getItem(key));
 const setLocalStorage = (key, value) => 
     window.localStorage.setItem(key, JSON.stringify(value));
 
 export const useStore = create((set) => ({
+    texture: 'dirt',
     cubes: getLocalStorage('world') || [],
-    addCube: (x, y, z, texture) => 
+    addCube: (x, y, z) => 
         set(state => ({
-            cubes: [...state.cubes, {pos: [x,y,z], texture}],
+            cubes: [
+                ...state.cubes,
+                { key: nanoid(), pos: [x, y, z], texture: state.texture },
+              ],
         })),
     removeCube: (x, y, z) => {
         set((state) => ({
